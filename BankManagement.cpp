71c9e85a9448c60d886b466ecb50f3895b072072
Shrived00 <shrivedkakde@gmail.com>
2025-01-07 00:00:27 +0530

    acc created

--- BankManagement.cpp
+++ BankManagement.cpp
@@ -27,16 +27,56 @@ public:
     }
     double getBalance()
     {
-        return balancel
+        return balance;
     }
 };
 
-class BankMan
-
-    int
-    main()
+class BankManagement
 {
-    cout << "Hello World";
+private:
+    vector<BankAccount> accounts;
+
+public:
+    void AddAccount(string name, int accountNum, double balance)
+    {
+        accounts.push_back(BankAccount(name, accountNum, balance));
+    }
+};
+
+int main()
+{
+    BankManagement bank;
+    int choice;
+    cout << "\t\t::Bank Management System" << endl;
+    cout << "\t\t\tMain Menu" << endl;
+    cout << "\t\t 1. Create New Account" << endl;
+    cout << "\t\t 2. Show All Account" << endl;
+    cout << "\t\t 3. Search Account" << endl;
+    cout << "\t\t 4. Deposit Money" << endl;
+    cout << "\t\t 5. Withdraw Money" << endl;
+    cout << "\t\t 6. Exit" << endl;
+    cout << "\t\t --------------------------------------" << endl;
+    cout << "\t\t Enter Your Choice :";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+    {
+        string name;
+        int accountNum;
+        double balance;
+        cout << "\t\t Enter Name: ";
+        cin >> name;
+        cout << "\t\t Enter Account NUmber: ";
+        cin >> accountNum;
+        cout << "\t\t Enter Initial Balance: ";
+        cin >> balance;
+        bank.AddAccount(name, accountNum, balance);
+        cout << "\t\t Account Created Successfulyl..." << endl;
+        break;
+    }
+    }
 
     return 0;
 }
