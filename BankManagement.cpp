f693ddd5d6095104d3766d2f0fe059c8129480e4
Shrived00 <shrivedkakde@gmail.com>
2025-01-06 23:27:02 +0530

    init

---
+++ BankManagement.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BankAccount
+{
+private:
+    string name;
+    int accountNum;
+    double balance;
+
+public:
+    BankAccount(string n, int ac, double bal)
+    {
+        name = n;
+        accountNum = ac;
+        balance = bal;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    int getAccountNum()
+    {
+        return accountNum;
+    }
+    double getBalance()
+    {
+        return balancel
+    }
+};
+
+class BankMan
+
+    int
+    main()
+{
+    cout << "Hello World";
+
+    return 0;
+}
